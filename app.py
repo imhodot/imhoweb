49c8914bb2c33188c7134499588841c4b583cf2a
Ama MGB <ama.mgbeoji@gmail.com>
2022-09-27 00:33:45 +0000

    UI change

--- app.py
+++ app.py
@@ -1,31 +1,72 @@
 #!/bin/env python3
 # app.py
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'IamHumble!'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    bio = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Student {self.username}>'
+
+# Views/Routes
 @app.route('/')
 @app.route('/index.html')
 def index():
     return render_template('index.html')
+
+@app.route('/home.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/about')
 @app.route('/about.html')
 def about():
     return render_template('about.html')
 
-@app.route('/blog')
-@app.route('/blog.html')
-def blog():
-    return render_template('blog.html')
-
 @app.route('/contact')
 @app.route('/contact.html')
 def contact():
     return render_template('contact.html')
+
+@app.route('/signup')
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/blog')
+@app.route('/blog.html')
+@app.route('/blog/<int:idx>')
+def blog(idx):
+    messages = ['Message Zero', 'Message One', 'Message Two']
+    try:
+        return render_template('blog.html', message=messages[idx])
+    except IndexError:
+        abort(404)
 
 # Error Handling
 @app.errorhandler(404)
@@ -33,9 +74,10 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def page_not_found(e):
+def internal_server_error(e):
     # note that we set the 404 status explicitly
     return render_template('500.html'), 500
+
 
 # Omittable
 if __name__ == "__main__":
