7bde2d23f22c3dfb18bd20199cc50e5bc019b59c
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2024-02-14 21:14:23 +0100

    made an update

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def signup():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Successful, you can login now!', category='succes')
+        flash('Successful, you can login now!', category='success')
         return redirect(url_for('login'))
 
         if confirm_password != password:
