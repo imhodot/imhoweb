7e56e436e1424962946f5b64dea8bd8f3b564308
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2024-02-29 08:54:39 +0100

    made an update

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
+login_manager.login_message_category = 'danger'
 
 
 @login_manager.user_loader
